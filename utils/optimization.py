99a6f3e7a038faca1e1a2e55e0f04992c07eb2cc
Alvaro Macias <amacias@afmaing.cl>
2025-12-05 15:56:52 -0300

    reorganice and clean version

--- utils/optimization.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-def optimization(df_m, df_c):
-  mt_c = np.matrix(df_c)
-  mt_m = np.matrix(df_m)
-  mt_delta = np.linalg.inv(mt_m.T @ mt_m) @ (mt_m.T @ mt_c)
-  return mt_delta
--- utils/read_data.py
+++ utils/read_data.py
@@ -1,39 +1,47 @@
-def read_electricity_prices(type_data='real'):
-    if type_data == 'real':
-        file_name = 'Electricity data ERCOT.csv'
-        df_electricity = read_data_csv(file_name)
-        df_electricity['Date'] = pd.to_datetime(df_electricity['Date'])
-    else:
-        file_name = 'Electricity data ERCOT.csv'
-        df_electricity = read_data_csv(file_name)
-        df_electricity['Date'] = pd.to_datetime(df_electricity['Date'])
+import os
+import pandas as pd
 
-    df_elec_prices = df_electricity[df_electricity['Date'].dt.hour == 0].pivot_table(index='Date', values='price', columns='zone')
-    outliers = [dt.date(2022, 12, 24)]
-    # df_prices.loc[dt.date(2021, 5, 1): dt.date(2022, 6, 1)].diff().hist()
-    df_elec_prices.loc[dt.date(2021, 5, 1): dt.date(2024, 6, 1)].plot()
-    df_electricy_prices = df_elec_prices.mean(axis=1).resample('ME').mean()
-    df_electricy_prices.plot()
-    
-    return df_electricity
+def read_data(data_path):
+  """
+  Reads and preprocesses Hashprice, BTC, and Electricity data from CSV files.
 
-def read_bitcoin_prices(type_data='real'):
-    if type_data == 'real':
-        file_name = 'BTC-USD.csv'
-        df_btc_usd = read_data_csv(file_name)
-        df_btc_usd.set_index('Date', inplace=True)
-        df_btc_usd.index = pd.to_datetime(df_btc_usd.index)
-        
-        df_btc_usd.rename(columns={'Adj Close': 'Price'}, inplace=True)
-        df_btc_usd = df_btc_usd[['Price']]
-        df_btc_usd = df_btc_usd.astype(float)
-    else:
-        file_name = 'BTC-USD.csv'
-        df_btc_usd = read_data_csv(file_name)
-        df_btc_usd.set_index('Date', inplace=True)
-        df_btc_usd.index = pd.to_datetime(df_btc_usd.index)
-        
-        df_btc_usd.rename(columns={'Adj Close': 'Price'}, inplace=True)
-        df_btc_usd = df_btc_usd[['Price']]
-        df_btc_usd = df_btc_usd.astype(float)
-    return df_btc_usd
+  Parameters:
+  data_path (str): Path to the directory containing the CSV files.
+
+  Returns:
+  pd.DataFrame: A combined DataFrame containing the preprocessed data for all assets.
+  """
+  files_pm = {}
+
+  files_pm['hashprice'] = {'file_name': 'bitcoin-hashprice-index-20251110.csv'}
+  files_pm['hashprice']['col_date_name'] = 'timestamp'
+  files_pm['hashprice']['col_price'] = 'usd_hashprice'
+
+  files_pm['btc'] = {'file_name': 'BTC-USD.csv'}
+  files_pm['btc']['col_date_name'] = 'Date'
+  files_pm['btc']['col_price'] = 'Adj Close'
+
+  files_pm['electricity'] = {'file_name': 'Electricity data ERCOT.csv'}
+  files_pm['electricity']['col_date_name'] = 'Date'
+  files_pm['electricity']['col_price'] = 'price'
+
+  df_data = pd.DataFrame([])
+  for key, pm in files_pm.items():
+    file_name = pm['file_name']
+    col_date_name = pm['col_date_name']
+    file_path = os.path.join(data_path, file_name)
+    df_data_aux = pd.read_csv(file_path)
+
+    df_data_aux[col_date_name] = pd.to_datetime(df_data_aux[col_date_name])
+    df_data_aux['date_aux'] = df_data_aux[col_date_name].dt.strftime('%Y-%m-%d')
+    df_data_aux['date_aux'] = pd.to_datetime(df_data_aux['date_aux'], format='%Y-%m-%d')
+    df_data_aux = df_data_aux.pivot_table(index='date_aux', values=pm['col_price'], aggfunc='mean')
+
+    df_data_aux = df_data_aux[[pm['col_price']]]
+    df_data_aux.rename(columns={pm['col_price']: key}, inplace=True)
+    df_data_aux.index.name = 'date'
+    df_data_aux.sort_index(inplace=True)
+
+    df_data = pd.concat([df_data, df_data_aux], axis=1)
+  df_data.dropna(inplace=True)
+  return df_data
